95b6f94a7067a440a84a27d5651cf8f57b669645
Wanderer134 <danilgusev981@gmail.com>
2024-11-26 12:48:04 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+immutable_var = 1 , 'Danil' , True
+print (immutable_var)
+immutable_var [0] = 2
+print (immutable_var)
+mutable_list = [1, 2, 'a', 'b', 'c']
+print (mutable_list)
+mutable_list [0] = 100
+print (mutable_list)
